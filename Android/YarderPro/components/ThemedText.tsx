fa80f851510326ecf170e9cd49f43c910b92f6b7
fcr15 <forrestcreid@yahoo.com>
2024-12-03 20:31:57 -0800

    Update ThemedText.tsx

--- Android/YarderPro/components/ThemedText.tsx
+++ Android/YarderPro/components/ThemedText.tsx
@@ -35,24 +35,29 @@ export function ThemedText({
 
 const styles = StyleSheet.create({
   default: {
+    fontFamily:'Inter',
     fontSize: 16,
     lineHeight: 24,
   },
   defaultSemiBold: {
+    fontFamily:'Inter',
     fontSize: 16,
     lineHeight: 24,
     fontWeight: '600',
   },
   title: {
+    fontFamily:'Inter',
     fontSize: 32,
     fontWeight: 'bold',
     lineHeight: 32,
   },
   subtitle: {
+    fontFamily:'Inter',
     fontSize: 20,
     fontWeight: 'bold',
   },
   link: {
+    fontFamily:'Inter',
     lineHeight: 30,
     fontSize: 16,
     color: '#0a7ea4',
